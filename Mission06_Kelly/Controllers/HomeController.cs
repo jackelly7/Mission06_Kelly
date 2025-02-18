1f0db7a48358b3c9bb7d973ec350c3e6751a7baa
jackelly7 <johnmcmillankelly@gmail.com>
2025-02-18 10:44:13 -0700

    fixed sqlite issues

--- Mission06_Kelly/Controllers/HomeController.cs
+++ Mission06_Kelly/Controllers/HomeController.cs
@@ -30,7 +30,6 @@ public class HomeController : Controller
     {
         return View();
     }
-
     [HttpPost]
 
     public IActionResult AddMovie(Movie movie)
@@ -38,17 +37,6 @@ public class HomeController : Controller
         _context.Movies.Add(movie);
         _context.SaveChanges();
         
-        return View("AddMovie", movie);
-    }
-
-    public IActionResult Privacy()
-    {
-        return View();
-    }
-
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        return View("Confirmation", movie);
     }
 }
--- Mission06_Kelly/Models/MoviesContext.cs
+++ Mission06_Kelly/Models/MoviesContext.cs
@@ -6,7 +6,6 @@ public class MoviesContext : DbContext
 {
     public MoviesContext(DbContextOptions<MoviesContext> options) : base(options) // constructor
     {
-        
     }
     
     public DbSet<Movie> Movies { get; set; }
